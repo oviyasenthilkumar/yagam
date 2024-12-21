c379e9fe5d0c9c5cd3695ef03cf554ad628b354d
oviyasenthilkumar <154113657+oviyasenthilkumar@users.noreply.github.com>
2024-12-21 16:46:17 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from 'react'
-import Home from './components/Home';
-import AboutMahaYagam from './components/About';
-import Header from './components/Header';
-import AboutTrust from './components/AboutTrust';
-import InvitationLetter from './components/Letter';
-import FundsSection from './components/Funds';
-import SacredGlimpses from './components/Carousel';
-import Footer from './components/Footer';
+// import React from 'react'
+
+
+// const App = () => {
+//   return (
+//     <>
+      // <Header />
+      // <Home />
+      // <AboutTrust />
+      // <SacredGlimpses />
+      // <InvitationLetter />
+      // <FundsSection />
+      // {/* <Footer /> */}
+//     </>
+//   );
+// };
+
+
+
+
+// export default App;
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Terms from "./Pages/T&C"; 
+import Home from "./Pages/Home"
+import ScrollToTop from "./components/ScrollToTop";
+import ContactUs from "./components/ContactUs";
+import ReturnAndCancellation from "./components/ReturnAndCancellation";
+import PrivacyPolicy from "./components/PrivacyPolicy";
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Home />
-      <AboutTrust />
-      <SacredGlimpses />
-      <InvitationLetter />
-      <FundsSection />
-      {/* <Footer /> */}
-    </>
+    <Router>
+      <div>
+        <ScrollToTop/>
+        <Routes>
+          {/* Define routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<PrivacyPolicy/>} />
+          <Route path="/return" element={<ReturnAndCancellation/>} />
+          <Route path="/contact" element={<ContactUs/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
-
-
-
 
 export default App;
